d7a40f1fefd58aa4e74784ab45e905679bcd26c9
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-11 04:48:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,40 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS=[
+  [0,1,2], [3,4,5], [6,7,8], 
+  [0,3,6], [1,4,7], [2,5,8], 
+  [0,4,8], [2,4,6]
+  ]
+  
+def won?(board)
+  if draw?(board)
+    return false
+  else
+    WIN_COMBINATIONS.detect do |winning_spots|
+      winning_spots.all? do |index|
+        board[index]=="X" && board[index]!="O" || board[index]=="O" && board[index]!="X"
+      end
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |index|
+    index=="X" || index=="O"
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if full?(board) || draw?(board) || won?(board)
+    return true
+  end
+end
